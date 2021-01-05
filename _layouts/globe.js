99ea7c4217b4825fbb359e0dba59d506e8b6ee6a
Nico <nicomontoya@hey.com>
2021-01-05 16:43:26 -0700

    Update globe.js

--- _layouts/globe.js
+++ _layouts/globe.js
@@ -111,7 +111,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir+'world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir+'earth-night.jpg');
 
     material = new THREE.ShaderMaterial({
 
